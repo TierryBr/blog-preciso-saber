6208b1ae6584f769572b34031e49cf454919db51
tierry <tierry@rastrosystem.com.br>
2023-05-21 00:25:43 -0300

    adjustments

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,9 @@
 pip install -r requirements.txt
 
 # make migrations
-python3.9 manage.py collectstatic --noinput
+python3.9 manage.py makemigrations
 python3.9 manage.py migrate
+python3.9 manage.py collectstatic --noinput
+
 
 
