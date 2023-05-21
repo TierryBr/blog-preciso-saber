5fac5e0dccef9f1cd6de1e4abf6896642b1cadcf
tierry <tierry@rastrosystem.com.br>
2023-05-20 23:01:44 -0300

    update vercel config

--- blog_preciso_saber/settings.py
+++ blog_preciso_saber/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 
